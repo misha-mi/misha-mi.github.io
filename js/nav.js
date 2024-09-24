bdd5496565fac5a92d1d112dbe75e5fa891e0e7e
misha-mi <misha.saighanov@mail.ru>
2024-09-24 18:38:55 +0600

    fix

--- js/nav.js
+++ js/nav.js
@@ -2,10 +2,23 @@ window.addEventListener("DOMContentLoaded", () => {
   const nav = document.querySelector(".nav");
   const burger = document.querySelector(".nav__burger");
 
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("nav__open");
-    nav.classList.contains("nav__open")
-      ? (document.body.style.overflow = "hidden")
-      : (document.body.style.overflow = "auto");
+  nav.addEventListener("click", (e) => {
+    const toggle = () => {
+      nav.classList.toggle("nav__open");
+      nav.classList.contains("nav__open")
+        ? (document.body.style.overflow = "hidden")
+        : (document.body.style.overflow = "auto");
+    };
+    if (
+      e.target.classList.contains("nav__burger") ||
+      e.target.classList.contains("nav__burger-elem")
+    ) {
+      toggle();
+    } else if (
+      e.target.classList.contains("nav__button") &&
+      nav.classList.contains("nav__open")
+    ) {
+      toggle();
+    }
   });
 });
