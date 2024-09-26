9eef0544d0f12cd8afecef37f26c8e8e431723a8
misha-mi <misha.saighanov@mail.ru>
2024-09-26 15:13:13 +0300

    fix

--- js/slider.js
+++ js/slider.js
@@ -15,8 +15,6 @@ window.addEventListener("DOMContentLoaded", () => {
     const buttonRight = slider.querySelector(".slider__right");
     let isActiveSlide = false;
 
-    window.addEventListener("resize", () => window.location.reload());
-
     if (sliderLine.clientWidth > slides[0].clientWidth * slides.length) {
       const countToFull = Math.trunc(
         sliderLine.clientWidth / slides[0].clientWidth
