7008ef9b4da6b34bee9cb6d6719a8f59f34be199
misha-mi <misha.saighanov@mail.ru>
2024-10-27 16:04:27 +0300

    safsadf

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,7 +1,38 @@
+import "./helpers/iframeLoader.js";
+import axios from "axios";
+import "./style.css";
+import { useEffect, useState } from "react";
+
 function App() {
+  let iframe;
+  const [currentPage, setCurrentPage] = useState("index.html");
+
+  useEffect(() => {
+    init("index.html");
+  }, []);
+
+  function init(page) {
+    iframe = document.querySelector("iframe");
+    open(page);
+    // Здесь можно добавить загрузку списка страниц
+  }
+
+  function open(page) {
+    setCurrentPage(page);
+    console.log(currentPage);
+  }
+
+  const createNewService = () => {
+    axios
+      .post("http://localhost/misha-mi.github.io/api/createServicePage.php", {
+        name: "something12",
+      })
+      .then(console.log);
+  };
+
   return (
     <div className="App">
-      <div>hello</div>
+      <iframe src="http://localhost/misha-mi.github.io"></iframe>
     </div>
   );
 }
--- api/createServicePage.php
+++ api/createServicePage.php
@@ -1,4 +1,15 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+// Если это запрос типа OPTIONS, завершите выполнение без обработки.
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit(0);
+}
+
+$_POST = json_decode( file_get_contents("php://input"), true );
 $newFile = "../service-pages/" . $_POST["name"] . ".html";
 
 if (file_exists($newFile)) {
